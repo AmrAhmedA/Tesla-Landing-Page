21b872527cf4be5c879aaf7e2b5e836d028b7389
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-06-05 21:54:45 +0200

    Add react scroll

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import HeaderContainer from './component/header';
-// import * as Scroll from 'react-scroll';
+import * as Scroll from 'react-scroll';
 import model_S_Desktop from "../src/assets/background/Model S/ms-homepage-desktop.jpg"
 import model_S_Mobile from "../src/assets/background/Model S/ms-homepage-mobile.jpg"
 import model_y_Desktop from "../src/assets/background/Model Y/Desktop-ModelY.jpg"
 import model_y_Mobile from "../src/assets/background/Model Y/Mobile-ModelY.jpg"
-
+let scroll = Scroll.animateScroll;
 const useStyles = makeStyles((theme) => ({
   landingContainer: {
     backgroundRepeat: "noRepeat",
@@ -34,20 +34,33 @@ function App() {
   const handleScroll = () => setOffSetY(window.pageYOffset);
 
   const classes = useStyles();
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [])
+
+  useEffect(() => {
+    if (offsetY === 100) {
+      scroll.scrollTo(600, { duration: "0.5s", smooth: true });
+      console.log("test")
+    }
+    else return
+  }, [offsetY]);
 
   console.log(offsetY);
   return (
     // <Container maxWidth="xl">
     <React.Fragment>
-
+      {/* <button onClick={() => {
+        scroll.scrollTo(600);
+      }}>Test</button> */}
       <div className={`${classes.landingContainer} ${classes.model_S_Container}`}>
         <HeaderContainer />
       </div>
       <div className={`${classes.landingContainer} ${classes.model_Y_Container}`}>
       </div>
+
     </React.Fragment>
     // </Container>
   );
