3c849ba8645f896dadd604c68a82988a5c6db299
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-06-06 17:22:13 +0200

    Add snap scrolling

--- src/App.js
+++ src/App.js
@@ -19,10 +19,32 @@ let scroller = Scroll.scroller;
 // let scroll = Scroll.animateScroll;
 const useStyles = makeStyles((theme) => ({
   landingContainer: {
+    overflowY: "scroll",
+    height: "100vh",
+    scrollSnapType: "y mandatory",
+    "&::-webkit-scrollbar": {
+      width: "0.4em",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "#eee9e9",
+      borderRadius: "10px",
+      boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+      webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.2)",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#0C4A70",
+      /* outline: 1px solid slategrey; */
+      borderRadius: "10px",
+    },
+  },
+  landingSection: {
+    minHeight: "100vh",
     backgroundRepeat: "noRepeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
     height: "100vh",
+    scrollSnapAlign: "start",
+
   },
   model_S_Container: {
     backgroundImage: `url(${model_S_Desktop})`,
@@ -63,29 +85,29 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 function App() {
-  const [offsetY, setOffSetY] = useState(0);
+  // const [offsetY, setOffSetY] = useState(0);
   const classes = useStyles();
 
-  const handleScroll = (e) => {
-    // console.log(e);
-    setOffSetY(window.pageYOffset)
-  };
+  // const handleScroll = (e) => {
+  //   // console.log(e);
+  //   setOffSetY(window.pageYOffset)
+  // };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, [])
 
-  useEffect(() => {
-    if (Math.round(offsetY) === 100) {
-      scroller.scrollTo("section2", {
-        delay: 100,
-        duration: 200,
-        smooth: true
-      })
-    }
-    console.log("test")
-  }, [offsetY]);
+  // useEffect(() => {
+  //   if (Math.round(offsetY) === 100) {
+  //     scroller.scrollTo("section2", {
+  //       delay: 100,
+  //       duration: 200,
+  //       smooth: true
+  //     })
+  //   }
+  //   console.log("test")
+  // }, [offsetY]);
 
 
   // const handlescrollin = () => {
@@ -104,21 +126,24 @@ function App() {
     <React.Fragment>
 
       {/* <div style={{ overflow: "hidden" }} > */}
-      <div
-        //  onScroll={handlescrollin()}
-        className={`${classes.landingContainer} ${classes.model_S_Container}`}>
-        <HeaderContainer />
+      <div className={classes.landingContainer}>
+        <div
+          //  onScroll={handlescrollin()}
+          className={`${classes.landingSection} ${classes.model_S_Container}`}>
+          <HeaderContainer />
+        </div>
+        <div id="section2" className={`${classes.landingSection} ${classes.model_Y_Container}`}>
+        </div>
+        <div id="section3" className={`${classes.landingSection} ${classes.model_3_Container}`}>
+        </div>
+        <div id="section4" className={`${classes.landingSection} ${classes.model_X_Container}`}>
+        </div>
+        <div id="section5" className={`${classes.landingSection} ${classes.us_Home}`}>
+        </div>
+        <div id="section6" className={`${classes.landingSection} ${classes.solar_Roof}`}>
+        </div>
       </div>
-      <div id="section2" className={`${classes.landingContainer} ${classes.model_Y_Container}`}>
-      </div>
-      <div id="section3" className={`${classes.landingContainer} ${classes.model_3_Container}`}>
-      </div>
-      <div id="section4" className={`${classes.landingContainer} ${classes.model_X_Container}`}>
-      </div>
-      <div id="section5" className={`${classes.landingContainer} ${classes.us_Home}`}>
-      </div>
-      <div id="section6" className={`${classes.landingContainer} ${classes.solar_Roof}`}>
-      </div>
+
       {/* </div> */}
     </React.Fragment>
     // </Container>
