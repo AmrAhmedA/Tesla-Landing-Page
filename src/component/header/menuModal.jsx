40f57bd7a249845b1e7547c2002aa982a8676223
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-07-22 15:43:45 +0200

    Add material ui icons + menu modal

--- src/component/header/menuModal.jsx
+++ src/component/header/menuModal.jsx
@@ -1,15 +1,36 @@
 import React from "react";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-
+import { IconButton, Fade, Backdrop, Modal, Grid } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 const MenuModal = ({ isVisible, setIsVisible }) => {
+  const menuItems = [
+    { name: "Existing Inventory" },
+    { name: "Used Inventory" },
+    { name: "Trade-In" },
+    { name: "Test Drive" },
+    { name: "Cybertruck0" },
+    { name: "Roadster" },
+    { name: "Semi" },
+    { name: "Charging" },
+    { name: "Powerwall" },
+    { name: "Commercial Energy" },
+    { name: "Utilities" },
+    { name: "Find Us" },
+    { name: "Support" },
+  ];
   return (
     <Modal
       aria-labelledby="transition-modal-title"
       aria-describedby="transition-modal-description"
+      style={{
+        display: "flex",
+        position: "fixed",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        // overflow: "auto",
+      }}
       open={isVisible}
       onClose={() => setIsVisible(false)}
+      disableAutoFocus
       closeAfterTransition
       BackdropComponent={Backdrop}
       BackdropProps={{
@@ -17,12 +38,41 @@ const MenuModal = ({ isVisible, setIsVisible }) => {
       }}
     >
       <Fade in={isVisible}>
-        <div>
-          <h2 id="transition-modal-title">Transition modal</h2>
-          <p id="transition-modal-description">
-            react-transition-group animates me.
-          </p>
-        </div>
+        <Grid
+          style={{
+            width: "311px",
+            height: "100vh",
+            maxWidth: "90vw",
+            position: "relative",
+            paddingBottom: "10px",
+            flexDirection: "column",
+            backgroundColor: "white",
+            borderRadius: "10px",
+            border: "none",
+            outline: "none",
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              padding: "32px 32px",
+            }}
+          >
+            <IconButton aria-label="delete">
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </div>
+          <div>
+            <ol style={{ listStyleType: "none" }}>
+              {menuItems.map((item, index) => (
+                <li>{item.name}</li>
+              ))}
+            </ol>
+          </div>
+        </Grid>
       </Fade>
     </Modal>
   );
